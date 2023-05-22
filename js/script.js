12461aff7cd82054340b8dda66ac18bac073739b
KamilObrzut <127477561+KamilObrzut@users.noreply.github.com>
2023-05-22 15:21:36 +0200

    Add Shuffling&Dealing and Marked func
    Add Shuffling&Dealing and Marked func

--- js/script.js
+++ js/script.js
@@ -1,76 +1,292 @@
-const calculateHeroSize = () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Global variables
   const heroArea = document.querySelector(".hero-area");
   const heroElements = document.querySelectorAll(".hero");
-  const windowWidth = window.innerWidth;
-
-  if (windowWidth < 667) {
-    const heroAreaWidth = heroArea.offsetWidth;
-    const heroAreaHeight = heroArea.offsetHeight;
-
-    const heroWidth = 0.8 * heroAreaHeight;
-    const heroHeight = 0.9 * heroAreaHeight;
-
-    heroElements.forEach((heroElement) => {
-      heroElement.style.width = `${heroWidth}px`;
-      heroElement.style.height = `${heroHeight}px`;
-    });
-  } else {
-    const heroAreaWidth = heroArea.offsetWidth;
-    const heroAreaHeight = heroArea.offsetHeight;
-
-    const heroWidth = 0.9 * heroAreaWidth;
-    const heroHeight = heroAreaWidth;
-
-    heroElements.forEach((heroElement) => {
-      heroElement.style.width = `${heroWidth}px`;
-      heroElement.style.height = `${heroHeight}px`;
-    });
-  }
-};
-calculateHeroSize();
-
-const calculateFontSize = () => {
   const textsToResize = document.querySelectorAll(".description");
-  const heroDivs = document.querySelectorAll(".hero");
+  const enemyDeck = document.querySelector(".enemy-deck");
+  const playerDeck = document.querySelector(".player-deck");
+  const enemyFightCards = document.querySelector(".enemy-fight-cards");
+  const playerFightCards = document.querySelector(".player-fight-cards");
 
-  heroDivs.forEach((heroDiv) => {
-    const heroDivWidth = heroDiv.offsetWidth;
+  // Card database
+  const baseCardsDatabase = [
+    {
+      name: "red-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "red-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "red-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "red-two-hit",
+      attack: 2,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "red-one-heal",
+      attack: 0,
+      heal: 1,
+      draw: 0,
+    },
+    {
+      name: "red-one-hit-one-draw",
+      attack: 1,
+      heal: 0,
+      draw: 1,
+    },
+    {
+      name: "blue-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "blue-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "blue-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "blue-two-hit",
+      attack: 2,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "blue-one-heal",
+      attack: 0,
+      heal: 1,
+      draw: 0,
+    },
+    {
+      name: "blue-one-hit-one-draw",
+      attack: 1,
+      heal: 0,
+      draw: 1,
+    },
+    {
+      name: "green-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "green-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "green-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "green-two-hit",
+      attack: 2,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "green-one-heal",
+      attack: 0,
+      heal: 1,
+      draw: 0,
+    },
+    {
+      name: "green-one-hit-one-draw",
+      attack: 1,
+      heal: 0,
+      draw: 1,
+    },
+    {
+      name: "yellow-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "yellow-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "yellow-one-hit",
+      attack: 1,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "yellow-two-hit",
+      attack: 2,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "yellow-one-heal",
+      attack: 0,
+      heal: 1,
+      draw: 0,
+    },
+    {
+      name: "yellow-one-hit-one-draw",
+      attack: 1,
+      heal: 0,
+      draw: 1,
+    },
+    {
+      name: "white-dodge",
+      attack: 0,
+      heal: 0,
+      draw: 0,
+    },
+    {
+      name: "white-two-draw",
+      attack: 0,
+      heal: 0,
+      draw: 2,
+    },
+  ];
 
-    textsToResize.forEach((textToResize) => {
-      const scale = heroDivWidth / 180;
-      const fontSize = 10 * scale;
+  // Interface Funcitions
+  const calculateHeroSize = () => {
+    const windowWidth = window.innerWidth;
 
-      textToResize.style.fontSize = `${fontSize}px`;
+    if (windowWidth < 667) {
+      const heroAreaWidth = heroArea.offsetWidth;
+      const heroAreaHeight = heroArea.offsetHeight;
+
+      const heroWidth = 0.8 * heroAreaHeight;
+      const heroHeight = 0.9 * heroAreaHeight;
+
+      heroElements.forEach((heroElement) => {
+        heroElement.style.width = `${heroWidth}px`;
+        heroElement.style.height = `${heroHeight}px`;
+      });
+    } else {
+      const heroAreaWidth = heroArea.offsetWidth;
+      const heroAreaHeight = heroArea.offsetHeight;
+
+      const heroWidth = 0.9 * heroAreaWidth;
+      const heroHeight = heroAreaWidth;
+
+      heroElements.forEach((heroElement) => {
+        heroElement.style.width = `${heroWidth}px`;
+        heroElement.style.height = `${heroHeight}px`;
+      });
+    }
+  };
+
+  const calculateFontSize = () => {
+    heroElements.forEach((heroDiv) => {
+      const heroDivWidth = heroDiv.offsetWidth;
+
+      textsToResize.forEach((textToResize) => {
+        const scale = heroDivWidth / 180;
+        const fontSize = 10 * scale;
+
+        textToResize.style.fontSize = `${fontSize}px`;
+      });
+    });
+  };
+
+  const calculateCardWidthLastChild = () => {
+    const lastCardInEnemyDeck = enemyDeck.lastElementChild;
+    const lastCardInPlayerDeck = playerDeck.lastElementChild;
+    const lastCardinEnemyFightCards = enemyFightCards.lastElementChild;
+    const lastCardinPlayerFightCards = playerFightCards.lastElementChild;
+
+    const cardWidth = lastCardInEnemyDeck ? lastCardInEnemyDeck.offsetHeight * 0.89 : 0;
+    const cardFightWidth = lastCardinEnemyFightCards ? lastCardinEnemyFightCards.offsetHeight * 0.89 : 0;
+
+    if (lastCardInEnemyDeck) lastCardInEnemyDeck.style.width = `${cardWidth}px`;
+    if (lastCardInPlayerDeck) lastCardInPlayerDeck.style.width = `${cardWidth}px`;
+    if (lastCardinEnemyFightCards) lastCardinEnemyFightCards.style.width = `${cardFightWidth}px`;
+    if (lastCardinPlayerFightCards) lastCardinPlayerFightCards.style.width = `${cardFightWidth}px`;
+
+    if (lastCardInEnemyDeck || lastCardInPlayerDeck || lastCardinEnemyFightCards || lastCardinPlayerFightCards) {
+      document.documentElement.style.setProperty("--card-width", `${cardWidth}px`);
+      document.documentElement.style.setProperty("--cardFight-width", `${cardFightWidth}px`);
+    }
+  };
+
+  if (enemyFightCards.childElementCount > 0 || playerFightCards.childElementCount > 0) {
+    calculateCardWidthLastChild();
+  }
+
+  // Shuffling and Dealing cards function
+  const createCardDivs = () => {
+    const remainingCards = [...baseCardsDatabase];
+
+    const playerCards = [];
+    const playerAttackValue = document.querySelector(".player-attack").textContent.slice(-2);
+    for (let i = 0; i < playerAttackValue; i++) {
+      const randomIndex = Math.floor(Math.random() * remainingCards.length);
+      const selectedCard = remainingCards.splice(randomIndex, 1)[0];
+      playerCards.push(selectedCard);
+    }
+
+    playerCards.forEach((card) => {
+      const cardDiv = document.createElement("div");
+      cardDiv.classList.add("card", card.name);
+      cardDiv.setAttribute("data-attack", card.attack);
+      cardDiv.setAttribute("data-heal", card.heal);
+      cardDiv.setAttribute("data-draw", card.draw);
+      playerDeck.appendChild(cardDiv);
+    });
+
+    const enemyCards = [];
+    const enemyAttackValue = document.querySelector(".enemy-attack").textContent.slice(-2);
+    for (let i = 0; i < enemyAttackValue; i++) {
+      const randomIndex = Math.floor(Math.random() * remainingCards.length);
+      const selectedCard = remainingCards.splice(randomIndex, 1)[0];
+      enemyCards.push(selectedCard);
+    }
+
+    enemyCards.forEach((card) => {
+      const cardDiv = document.createElement("div");
+      cardDiv.classList.add("card", card.name);
+      cardDiv.setAttribute("data-attack", card.attack);
+      cardDiv.setAttribute("data-heal", card.heal);
+      cardDiv.setAttribute("data-draw", card.draw);
+      enemyDeck.appendChild(cardDiv);
+    });
+  };
+  createCardDivs();
+
+  // Marking cards function
+  const allCards = document.querySelectorAll(".card");
+  allCards.forEach((card) => {
+    card.addEventListener("click", (event) => {
+      event.target.classList.toggle("marked");
     });
   });
-};
-calculateFontSize();
 
-const calculateCardWidthLastChild = () => {
-  const enemyDeck = document.querySelector(".enemy-deck");
-  const lastCardInEnemyDeck = enemyDeck.lastElementChild;
-  const playerDeck = document.querySelector(".player-deck");
-  const lastCardInPlayerDeck = playerDeck.lastElementChild;
-  const enemyFightCards = document.querySelector(".enemy-fight-cards");
-  const lastCardinEnemyFightCards = enemyFightCards.lastElementChild;
-  const playerFightCards = document.querySelector(".player-fight-cards");
-  const lastCardinPlayerFightCards = playerFightCards.lastElementChild;
+  calculateHeroSize();
+  calculateFontSize();
+  calculateCardWidthLastChild();
 
-  const cardWidth = lastCardInEnemyDeck.offsetHeight * 0.89;
-  const cardFightWidth = lastCardinEnemyFightCards.offsetHeight * 0.89;
-  lastCardInEnemyDeck.style.width = `${cardWidth}px`;
-  lastCardInPlayerDeck.style.width = `${cardWidth}px`;
-  lastCardinEnemyFightCards.style.width = `${cardFightWidth}px`;
-  lastCardinPlayerFightCards.style.width = `${cardFightWidth}px`;
-
-  document.documentElement.style.setProperty("--card-width", `${cardWidth}px`);
-  document.documentElement.style.setProperty(
-    "--cardFight-width",
-    `${cardFightWidth}px`
-  );
-};
-calculateCardWidthLastChild();
-
-window.addEventListener("resize", calculateHeroSize);
-window.addEventListener("resize", calculateFontSize);
-window.addEventListener("resize", calculateCardWidthLastChild);
+  window.addEventListener("resize", calculateHeroSize);
+  window.addEventListener("resize", calculateFontSize);
+  window.addEventListener("resize", calculateCardWidthLastChild);
+});
