210bcbcc48780d0a49bf5bd22e5f0ed7196a352e
KamilObrzut <127477561+KamilObrzut@users.noreply.github.com>
2023-05-29 16:00:02 +0200

    Toggle Turn, Calculate DMG
    Added toggle turn for player - enemy and added calculate DMG

--- js/script.js
+++ js/script.js
@@ -282,6 +282,10 @@ document.addEventListener("DOMContentLoaded", () => {
       const cardClassList = event.target.classList;
       const cardColor = cardClassList[1].split("-")[0];
 
+      if (btnFightEnemy.classList.contains("disable")) {
+        return;
+      }
+
       if (enemyMarkedCards.length === 0) {
         enemyMarkedCards.push(card);
         cardClassList.add("marked");
@@ -299,6 +303,10 @@ document.addEventListener("DOMContentLoaded", () => {
     card.addEventListener("click", (event) => {
       const cardClassList = event.target.classList;
       const cardColor = cardClassList[1].split("-")[0];
+
+      if (btnFightPlayer.classList.contains("disable")) {
+        return;
+      }
 
       if (playerMarkedCards.length === 0) {
         playerMarkedCards.push(card);
@@ -358,12 +366,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const heroPlayer = document.querySelector(".player-hero");
 
   const enemyAnimationAttack = () => {
+    if (enemyFightCards.childElementCount === 0) {
+      return;
+    }
     heroEnemy.classList.add("enemy-hero-attack-animation");
     setTimeout(() => {
       heroEnemy.classList.remove("enemy-hero-attack-animation");
     }, 1000);
   };
   const playerAnimationAttack = () => {
+    if (playerFightCards.childElementCount === 0) {
+      return;
+    }
     heroPlayer.classList.add("player-hero-attack-animation");
     setTimeout(() => {
       heroPlayer.classList.remove("player-hero-attack-animation");
@@ -372,6 +386,117 @@ document.addEventListener("DOMContentLoaded", () => {
 
   btnFightEnemy.addEventListener("click", enemyAnimationAttack);
   btnFightPlayer.addEventListener("click", playerAnimationAttack);
+
+  //FirstTurn Function
+  const firstTurn = () => {
+    const enemySpeedValue = document.querySelector(".enemy-speed").textContent.slice(-1);
+    const playerSpeedValue = document.querySelector(".player-speed").textContent.slice(-1);
+
+    if (enemySpeedValue > playerSpeedValue) {
+      btnFightPlayer.classList.add("disable");
+    } else {
+      btnFightEnemy.classList.add("disable");
+    }
+  };
+  firstTurn();
+
+  //Toggle fight button
+  const toggleButtons = () => {
+    btnFightEnemy.classList.toggle("disable");
+    btnFightPlayer.classList.toggle("disable");
+  };
+
+  //Fight functions
+  const fightEnemy = () => {
+    if (enemyFightCards.childElementCount === 0) {
+      return;
+    }
+
+    let totalAttack = 0;
+    let totalHeal = 0;
+    let totalDraw = 0;
+
+    enemyFightCards.childNodes.forEach((card) => {
+      const attack = parseInt(card.getAttribute("data-attack"));
+      const heal = parseInt(card.getAttribute("data-heal"));
+      const draw = parseInt(card.getAttribute("data-draw"));
+
+      totalAttack += attack;
+      totalHeal += heal;
+      totalDraw += draw;
+    });
+
+    console.log("Atak: ", totalAttack);
+    console.log("Heal: ", totalHeal);
+    console.log("Draw: ", totalDraw);
+
+    while (enemyFightCards.firstChild) {
+      enemyFightCards.removeChild(enemyFightCards.firstChild);
+    }
+
+    const changeHealth = () => {
+      let playerHealthElement = document.querySelector(".player-health");
+      let playerHealth = parseInt(playerHealthElement.textContent.slice(-1));
+      let playerNewHealth = playerHealth - totalAttack;
+      playerHealthElement.textContent = "Zdrowie: " + playerNewHealth;
+      playerHealthElement.style.color = "red";
+
+      setTimeout(() => {
+        playerHealthElement.style.color = "";
+      }, 2000);
+    };
+    changeHealth();
+
+    enemyMarkedCards = [];
+    toggleButtons();
+  };
+
+  const fightPlayer = () => {
+    if (playerFightCards.childElementCount === 0) {
+      return;
+    }
+
+    let totalAttack = 0;
+    let totalHeal = 0;
+    let totalDraw = 0;
+
+    playerFightCards.childNodes.forEach((card) => {
+      const attack = parseInt(card.getAttribute("data-attack"));
+      const heal = parseInt(card.getAttribute("data-heal"));
+      const draw = parseInt(card.getAttribute("data-draw"));
+
+      totalAttack += attack;
+      totalHeal += heal;
+      totalDraw += draw;
+    });
+
+    console.log("Atak: ", totalAttack);
+    console.log("Heal: ", totalHeal);
+    console.log("Draw: ", totalDraw);
+
+    while (playerFightCards.firstChild) {
+      playerFightCards.removeChild(playerFightCards.firstChild);
+    }
+
+    const changeHealth = () => {
+      let enemyHealthElement = document.querySelector(".enemy-health");
+      let enemyHealth = parseInt(enemyHealthElement.textContent.slice(-1));
+      let enemyNewHealth = enemyHealth - totalAttack;
+      enemyHealthElement.textContent = "Zdrowie: " + enemyNewHealth;
+      enemyHealthElement.style.color = "red";
+
+      setTimeout(() => {
+        enemyHealthElement.style.color = "";
+      }, 2000);
+    };
+    changeHealth();
+
+    playerMarkedCards = [];
+    toggleButtons();
+  };
+
+  btnFightPlayer.addEventListener("click", fightPlayer);
+  btnFightEnemy.addEventListener("click", fightEnemy);
 
   window.addEventListener("resize", calculateHeroSize);
   window.addEventListener("resize", calculateFontSize);
